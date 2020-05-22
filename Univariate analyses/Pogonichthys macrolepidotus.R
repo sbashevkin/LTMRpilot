1c28ba72ed62813294d6d9ab0e5ea890af5c6b38
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2020-05-22 16:00:14 -0700

    More work on univariate models

--- Univariate analyses/Pogonichthys macrolepidotus.R
+++ Univariate analyses/Pogonichthys macrolepidotus.R
@@ -105,8 +105,28 @@ m219 <- lmer(CPUE^(1/3) ~ Year_fac + Month_fac + (1|Station), data=Data2)
 # CPUE^(1/3), numeric year, tw family
 m220<- bam(CPUE^(1/3) ~ s(Year_s) + s(Julian_day_s) + s(Station_fac, bs="re"), family=tw, data=Data2, method="fREML", discrete=F, nthreads=4)
 
-fit_plot<-function(model, X, title, subtitle){
-  fit<-fitted(model)
+# round(Count), Tow_area, numeric year, average length, zero-inflated poisson with salinity predicting presence
+m221<- gam(list(as.integer(round(Count)) ~ Tow_area_s + s(Year_s) + s(Julian_day_s) + s(Length_s) + s(Station_fac, bs="re"), ~s(Sal_surf_s)), family=ziplss, data=mutate(Data2, Length_s=replace_na(Length_s, 0)), method="REML")
+
+# CPUE^(1/3), numeric year, year and julian_day by station, tw family
+m222<- bam(CPUE^(1/3) ~ s(Year_s, by=Station_fac) + s(Julian_day_s, by=Station_fac) + s(Station_fac, bs="re"), family=tw, data=Data2, method="fREML", discrete=T, nthreads=4)
+
+# CPUE^(1/3), factor year and station with interaction, julian_day by station, tw family
+m223<- bam(CPUE^(1/3) ~ Year_fac*Station_fac + s(Julian_day_s, by=Station_fac), family=tw, data=Data2, method="fREML", discrete=T, nthreads=4)
+
+# CPUE^(1/3), 3D smooth with julian day, factor year, and factor station, tw family
+m224 <- bam(CPUE^(1/3) ~ te(Julian_day_s, Year_fac, Station_fac, bs=c("cc", "fs", "fs")), family=tw, data=Data2, method="fREML", discrete=T, nthreads=4)
+
+# CPUE^(1/3), 3D smooth with julian day, factor year, and factor station, tw family
+m225 <- bam(CPUE^(1/3) ~ te(Julian_day_s, Year_fac, Station_fac, bs=c("cc", "re", "re")), family=tw, data=Data2, method="fREML", discrete=T, nthreads=4)
+
+fit_plot<-function(model, X, title, subtitle, type="fit"){
+  if(type=="fit"){
+    fit<-fitted(model, type="response")
+  }
+  if(type=="predict"){
+    fit<-predict(model, type="response")
+  }
   p<-ggplot(data=Data2)+
     geom_point(aes(x=X, y=fit), alpha=0.2)+
     geom_abline(intercept = 0, slope=1, color="red", size=1)+
@@ -121,7 +141,7 @@ fit_plot<-function(model, X, title, subtitle){
 
 
 
-p<-pmap(list(model=list(m2, m22, m23, m24, m25, m26, m27, m28, m29, m210, m211, m212, m213, m214, m215, m216, m217, m218$gam, m219, m220),
+p<-pmap(list(model=list(m2, m22, m23, m24, m25, m26, m27, m28, m29, m210, m211, m212, m213, m214, m215, m216, m217, m218$gam, m219, m220, m221, m222, m223, m224),
              X=list(log(Data2$CPUE+1), 
                     Data2$CPUE, 
                     Data2$Count, 
@@ -140,6 +160,10 @@ p<-pmap(list(model=list(m2, m22, m23, m24, m25, m26, m27, m28, m29, m210, m211, 
                     as.integer(round(Data2$Count)),
                     Data2$Count,
                     log(Data2$CPUE+1),
+                    Data2$CPUE^(1/3),
+                    Data2$CPUE^(1/3),
+                    as.integer(round(Data2$Count)),
+                    Data2$CPUE^(1/3),
                     Data2$CPUE^(1/3),
                     Data2$CPUE^(1/3)),
              title=list('log(CPUE+1), numeric year, tw family', 
@@ -161,7 +185,11 @@ p<-pmap(list(model=list(m2, m22, m23, m24, m25, m26, m27, m28, m29, m210, m211, 
                         'Count, Tow_area, Spatiotemporal smoothers, tw family',
                         'log(CPUE+1), numeric year, tw family, temporal autocorrelation',
                         'Simple linear mixed effects model',
-                        'CPUE^(1/3), numeric year, tw family'),
+                        'CPUE^(1/3), numeric year, tw family',
+                        'round(Count), Tow_area, numeric year, average length, zero-inflated poisson with salinity predicting presence',
+                        'CPUE^(1/3), numeric year, year and julian_day by station, tw family',
+                        'CPUE^(1/3), factor year and station with interaction, julian_day by station, tw family',
+                        'CPUE^(1/3), 3D smooth with julian day, factor year, and factor station, tw family'),
              subtitle=c('bam(log(CPUE+1) ~ s(Year_s) + s(Julian_day_s) + s(Station_fac, bs="re"), family=tw)',
                         'bam(CPUE ~ s(Year) + s(Julian_day) + s(Station_fac, bs="re"), family=tw)',
                         'bam(Count ~ Tow_area_s + s(Year_s) + s(Julian_day_s) + s(Station_fac, bs="re"), family=tw)',
@@ -181,18 +209,41 @@ p<-pmap(list(model=list(m2, m22, m23, m24, m25, m26, m27, m28, m29, m210, m211, 
                         'bam(Count ~ Tow_area_s + te(Latitude_s, Longitude_s, Year_s, d=c(2,1), k=c(20, 10)) + s(Julian_day_s, bs="cc"), family=tw',
                         'gamm(log(CPUE+1) ~ s(Year_s) + s(Julian_day_s), random =list(Station_fac=~1), family=tw, correlation = corCAR1(form = ~ Date|Station))',
                         'lmer(CPUE^(1/3) ~ Year_fac + Month_fac + (1|Station))',
-                        'bam(CPUE^(1/3) ~ s(Year_s) + s(Julian_day_s) + s(Station_fac, bs="re"), family=tw')),
+                        'bam(CPUE^(1/3) ~ s(Year_s) + s(Julian_day_s) + s(Station_fac, bs="re"), family=tw)',
+                        'gam(list(as.integer(round(Count)) ~ Tow_area_s + s(Year_s) + s(Julian_day_s) + s(Length_s) + s(Station_fac, bs="re"), ~s(Sal_surf_s)), family=ziplss)',
+                        'bam(CPUE^(1/3) ~ s(Year_s, by=Station_fac) + s(Julian_day_s, by=Station_fac) + s(Station_fac, bs="re"), family=tw)',
+                        'bam(CPUE^(1/3) ~ Year_fac*Station_fac + s(Julian_day_s, by=Station_fac), family=tw)',
+                        'bam(CPUE^(1/3) ~ te(Julian_day_s, Year_fac, Station_fac, bs=c("cc", "fs", "fs")), family=tw)'),
+             type=c(rep("fit", 20), "predict", rep("fit", 3))),
         fit_plot)
 
 
-walk2(p, paste("model", 1:20), ~ggsave(filename = paste0("Univariate analyses/Figures/", .y, ".png"), plot = .x, device = "png", width=15, height=6, units="in"))
+walk2(p, paste("model", 1:24), ~ggsave(filename = paste0("Univariate analyses/Figures/", .y, ".png"), plot = .x, device = "png", width=15, height=6, units="in"))
 
 
 
 
 
+# Including length --------------------------------------------------------
+
+Data_length <- LTMRpilot(convert_lengths=TRUE, remove_unconverted_lengths = TRUE, size_cutoff=40)%>%
+  zero_fill(species="Pogonichthys macrolepidotus", remove_unknown_lengths = TRUE, univariate = TRUE)%>%
+  filter(Method=="Otter trawl")%>%
+  drop_na(Sal_surf, Temp_surf, Latitude, Longitude, Date, Tow_area)%>%
+  mutate(Julian_day=yday(Date),
+         Year=year(Date),
+         Month=month(Date),
+         CPUE=Count/Tow_area,
+         Station_fac=factor(Station),
+         Year_fac=ordered(Year),
+         Month_fac=ordered(Month))%>%
+  mutate_at(vars(Length, Longitude, Latitude, Year, Julian_day, Sal_surf, Temp_surf, Tow_area), list(s=~(.-mean(., na.rm=T))/sd(., na.rm=T)))%>% # Create centered and standardized versions of covariates
+  mutate(Length_s=replace_na(Length_s, 0))%>%
+  filter(Station%in%Stations$Station)
 
 
+# Length
+m221<- gam(list(as.integer(round(Count)) ~ s(Year_s) + s(Julian_day_s, bs="cc") + s(Length_s, k=100) + s(Station_fac, bs="re"), ~s(Sal_surf_s)), family=ziplss, data=Data_length, method="REML")
 
 
 
@@ -262,14 +313,7 @@ mbnb<- brm(as.integer(round(Count)) ~ Tow_area_s + t2(Latitude_s, Longitude_s, Y
            chains=1, cores=1,
            iter = iterations, warmup = warmup)
 
-Data_length <- LTMRpilot(convert_lengths=TRUE, remove_unconverted_lengths = TRUE, size_cutoff=40)%>%
-  zero_fill(species="Pogonichthys macrolepidotus", remove_unknown_lengths = TRUE, univariate = TRUE)%>%
-  drop_na(Sal_surf, Temp_surf, Latitude, Longitude, Date, Tow_area)%>%
-  mutate(Julian_day=yday(Date),
-         Year=year(Date),
-         Month=month(Date))%>%
-  mutate_at(vars(Longitude, Latitude, Year, Julian_day, Sal_surf, Temp_surf, Tow_area, Length), list(s=~(.-mean(., na.rm=T))/sd(., na.rm=T)))%>% # Create centered and standardized versions of covariates
-  mutate(Length_s=replace_na(Length_s, 0))
+## Bayesian length model
 
 mbayes4<- brm(bf(as.integer(round(Count)) ~ Tow_area_s + t2(Latitude_s, Longitude_s, Year_s, d=c(2,1), k=c(15, 10)) + s(Julian_day_s, bs="cc") + Source + poly(Sal_surf_s, 2) + s(Temp_surf_s) + Length_s,
                  hu~poly(Sal_surf_s, 2)),
